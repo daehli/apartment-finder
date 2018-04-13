e68dfb14abd7d0a19e38652fc59bbce64b3d8928
Daehli <daehlinadeauotis@gmail.com>
2018-04-12 23:04:23 -0400

    add node_module files

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/notifications', async(req,resp)=>{
 		Object.assign(item,results)
 		if(item['neighborhood'].length != 0 || item['stations'].length != 0){
 			const data = await Object.assign({},{lat:item['geo:lat'],long:item['geo:long'],price:item['g-core:price']})
-				const isNotDuplicated = await insert(data)
+				const isNotDuplicated = await insert(data).catch((resolve => resolve))
 				if(isNotDuplicated){
 					let commuting = await transitTime([item['geo:lat'],item['geo:long']])
 					collection.push(Object.assign(item,{'commuting':commuting}))
@@ -52,3 +52,5 @@ app.get('/notifications1', async(req,resp)=>{
 	PostListingToSlack(data)
 	resp.json(data)
 })
+
+// [{"title":"Condo 4 1/2 meubl&#233;. Le Plateau Mont-Royal, 5 min m&#233;tro Laurier.","link":"https://www.kijiji.ca/v-appartement-condo-4-1-2/ville-de-montreal/condo-4-1-2-meuble-le-plateau-mont-royal-5-min-metro-laurier/1344244626","description":"Condo 4 1/2 meubl&#233;. Le Plateau Mont-Royal, 5 min m&#233;tro Laurier. Spacieux condo moderne 4 &#189; r&#233;nov&#233; en 2017 avec un grand salon, une chambre &#224; coucher ferm&#233;e et un bureau avec une sofa, plafond 9 pi. ...","enclosure":"","pubDate":"Mon, 02 Apr 2018 22:33:08 GMT","guid":"https://www.kijiji.ca/v-appartement-condo-4-1-2/ville-de-montreal/condo-4-1-2-meuble-le-plateau-mont-royal-5-min-metro-laurier/1344244626","dc:date":"2018-04-02T22:33:08Z","geo:lat":"45.5243055","geo:long":"-73.5851096","g-core:price":"1700.0","innerAd":{},"neighborhood":[{"found":true,"name":"Plateau"}],"stations":[{"found":true,"name":"Rosemount","distance":1.25},{"found":true,"name":"Laurier","distance":0.48},{"found":true,"name":"Mont-Royal","distance":0.31}]}]
