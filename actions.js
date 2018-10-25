0accf9f29372ac6254c20ff8309124bedc9d8c2f
daehli <daehlinadeauotis@gmail.com>
2018-10-24 21:10:02 -0400

    Linter correction

--- actions.js
+++ actions.js
@@ -2,36 +2,45 @@ const Promise = require('bluebird')
 const db = require('./knexfile')
 const knex = require('knex')(db[process.env.NODE_ENV])
 const slack = require('./slack')
-const kijiji = require("kijiji-scraper")
+const kijiji = require('kijiji-scraper')
 const SETTINGS = require('./settings')
 
 const insert = async data => {
-	return new Promise((resolve,reject)=>{
-		knex('Listing').insert({"kv":JSON.stringify(data)}).then(data=>{
-			resolve(true)
-		}).catch((err) =>{
-			reject(err)
-		})
-	})
-}
-
-const query = async (pref_obj,params_obj) => {
-        return new Promise((resolve,reject)=>{ 
-            kijiji.query(pref_obj,params_obj, async (err, ad) => {
-                if(err){
-                    reject(err)
-                }
-                resolve(ad)
+    return new Promise((resolve, reject) => {
+        knex('Listing')
+            .insert({ kv: JSON.stringify(data) })
+            .then(data => {
+                resolve(true)
             })
-        })
-};
-
-const postListingToSlack = (listing,channel)=> {
-    listing.map(async x=>{
-        const desc = `🍩 ${x['title']}  | 💰${x['g-core:price']} \n 🔗 <${x['link']}>`
-        slack.chat.postMessage({ channel: `#${channel}` , username: SETTINGS.USERNAME, icon_emoji:":robot_face:", text:desc, unfurl_links: true, unfurl_media: true })
+            .catch(err => {
+                reject(err)
+            })
     })
 }
 
+const query = async (pref_obj, params_obj) => {
+    return new Promise((resolve, reject) => {
+        kijiji.query(pref_obj, params_obj, async (err, ad) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(ad)
+        })
+    })
+}
 
-module.exports = { insert , query, postListingToSlack };
+const postListingToSlack = (listing, channel) => {
+    listing.map(async x => {
+        const desc = `🍩 ${x['title']}  | 💰${x['g-core:price']} \n 🔗 <${x['link']}>`
+        slack.chat.postMessage({
+            channel: `#${channel}`,
+            username: SETTINGS.USERNAME,
+            icon_emoji: ':robot_face:',
+            text: desc,
+            unfurl_links: true,
+            unfurl_media: true
+        })
+    })
+}
+
+module.exports = { insert, query, postListingToSlack }
--- router.js
+++ router.js
@@ -1,36 +1,34 @@
 const Promise = require('bluebird')
-let express = require('express');
-let router = express.Router();
+let express = require('express')
+let router = express.Router()
 let pick = require('lodash/pick')
 let map = require('lodash/map')
-const { findPointOfInterest,transitTime} = require('./utils')
-const { insert,query,postListingToSlack } = require('./actions')
-const { prefs_obj,params_obj,simpleKeysValueObj } = require('./models')
+const { findPointOfInterest, transitTime } = require('./utils')
+const { insert, query, postListingToSlack } = require('./actions')
+const { prefs_obj, params_obj, simpleKeysValueObj } = require('./models')
 
-
-
-router.get('/:channel/kijiji', async(req,resp)=>{
+router.get('/:channel/kijiji', async (req, resp) => {
 	let params = req.query
 	let channelName = req.params.channel
-	
-	let parameters = pick(params,Object.keys(params_obj))
-	let preference = pick(params,Object.keys(prefs_obj))
-	
-	let kijijiQuery = await query(preference,parameters)
-	let data = await kijijiQuery.reduce(async(previousPromise,item) => {
-		let collection = await previousPromise;
-		let kv = pick(item,Object.keys(simpleKeysValueObj))
-		const isNotDuplicated = await insert(item).catch((resolve => resolve))
-		if (isNotDuplicated === true){
-			collection.push(item);
+
+	let parameters = pick(params, Object.keys(params_obj))
+	let preference = pick(params, Object.keys(prefs_obj))
+
+	let kijijiQuery = await query(preference, parameters)
+	let data = await kijijiQuery.reduce(async (previousPromise, item) => {
+		let collection = await previousPromise
+		let kv = pick(item, Object.keys(simpleKeysValueObj))
+		const isNotDuplicated = await insert(item).catch(resolve => resolve)
+		if (isNotDuplicated === true) {
+			collection.push(item)
 		}
 		return collection
-		},Promise.resolve([]))
-	postListingToSlack(data,channelName)
+	}, Promise.resolve([]))
+	postListingToSlack(data, channelName)
 	resp.json(data)
 })
 
-module.exports = router;
+module.exports = router
 
 // curl 'localhost:4001/api/road-bikes/kijiji?locationId=1700281&categoryId=648&maxPrice=500'
 // curl 'localhost:4001/api/bike-fixies/kijiji?locationId=1700281&categoryId=15096002&maxPrice=500'
